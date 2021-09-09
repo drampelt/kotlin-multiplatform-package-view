b2db3baf58b09ca5890e95765ebec9c56a0a9446
GitHub Action <action@github.com>
2021-09-09 21:00:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/drampelt/kotlinmultiplatformpackageview/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/drampelt/kotlinmultiplatformpackageview/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.drampelt.kotlinmultiplatformpackageview.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.drampelt.kotlinmultiplatformpackageview.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/drampelt/kotlinmultiplatformpackageview/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.drampelt.kotlinmultiplatformpackageview.services
+
+import com.github.drampelt.kotlinmultiplatformpackageview.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
